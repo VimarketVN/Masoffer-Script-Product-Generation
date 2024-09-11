5950259813d500ef38fcf13b6bcf293e60ff23fe
Nguyễn Anh Tú <tu.na@Tunas-MacBook-Pro.local>
2024-09-11 09:53:35 +0700

    Product Generation: Dantri.vn

---
+++ dantri.js
@@ -0,0 +1,57 @@
+// Cấu hình script
+let publisherAPIToken = "gKICt6SQG8Fuad7JTZmqQw";
+let productListContainer =
+  document.querySelector("#mobile-article-bottom") ||
+  document.querySelector("#desktop-article-bottom");
+
+// Hàm xử lý lấy keyword tự động
+const keywords = (function () {
+  return Array.from(
+    document.querySelectorAll(".tags-wrap a[data-content-name='article-tags']")
+  )
+    .slice(0, 3)
+    .map((i) => i.innerText.trim());
+})();
+
+const getProductInfo = async (url, category, keywords) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authentication", "waXTDuEAOHIfNTXnNmFXBMweoYM2z4");
+  myHeaders.append("Content-Type", "application/json");
+  const raw = JSON.stringify({
+    post_url: url,
+    category: category,
+    container_id: productListContainer.getAttribute("id"),
+    publisher_key: publisherAPIToken,
+    keyword: keywords,
+  });
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+  fetch("https://api.masoffer.com/webhook/product-generation", requestOptions)
+    .then((response) => response.json())
+    .then(({ list_id, publisher_id }) => {
+      let container = document.createElement("div");
+      container.id = `mo-product-list-${list_id}`;
+      productListContainer.appendChild(container);
+      let script = document.createElement("script");
+      script.src = `https://ecom.masoffer.com/api/generate-component?publiser_id=${publisher_id}&list_id=${list_id}`;
+      script.type = "text/javascript";
+      script.defer = true;
+      document.body.appendChild(script);
+    })
+    .catch((error) => error);
+};
+
+if (productListContainer) {
+  let currentUrl = new URL(window.location.href);
+  let url = `${currentUrl.origin}${currentUrl.pathname}`;
+  let category = currentUrl.pathname.split("/").filter((i) => i)[0];
+  let chinhtriCategory = document.querySelector("a[data-content-target='/xa-hoi/chinh-tri.htm']");
+  if (!chinhtriCategory) {
+    getProductInfo(url, category, keywords).then((r) => r);
+  }
+}
+// Kết thúc cấu hình
